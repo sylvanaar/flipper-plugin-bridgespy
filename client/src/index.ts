bb561fb9ec7bcfc53c3bf255b4c61982d33c6a8d
Jon S Akhtar <sylvanaar@mindspring.com>
2021-01-02 02:22:36 -0800

    🚑 Fix circular reference errors from stringify

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,6 @@
 import { addPlugin, Flipper } from "react-native-flipper";
 import MessageQueue from "react-native/Libraries/BatchedBridge/MessageQueue";
+import stringifySafe from "./stringifySafe";
 
 const TO_JS = 0;
 let id = 0;
@@ -8,44 +9,44 @@ let _connection: Flipper.FlipperConnection | undefined;
 const buffer: any[] = [];
 const BATCH_SIZE = 50;
 addPlugin({
-    getId() {
-        return "com.sylvanaar.flipper.bridgespy";
-    },
-    onConnect(connection) {
-        _connection = connection;
-        console.log("connected");
+  getId() {
+    return "com.sylvanaar.flipper.bridgespy";
+  },
+  onConnect(connection) {
+    _connection = connection;
+    console.log("connected");
 
-        MessageQueue.spy((info: any) => {
-            if (info.module !== "Flipper" && _connection) {
-                info.time = Date.now();
-                info.id = (id++).toString();
-                info.type = info.type === TO_JS ? "N->JS" : "JS->N";
+    MessageQueue.spy((info: any) => {
+      if (info.module !== "Flipper" && _connection) {
+        info.time = Date.now();
+        info.id = (id++).toString();
+        info.type = info.type === TO_JS ? "N->JS" : "JS->N";
 
-                buffer.push(info);
-                if (buffer.length === 1 || buffer.length === BATCH_SIZE)
-                    setTimeout(() => {
-                        try {
-                            _connection?.send("newRow", buffer);
-                        } finally {
-                            buffer.length = 0;
-                        }
-                    }, 50);
+        buffer.push(info);
+        if (buffer.length === 1 || buffer.length === BATCH_SIZE)
+          setTimeout(() => {
+            try {
+              _connection?.send("newRow", stringifySafe(buffer));
+            } finally {
+              buffer.length = 0;
             }
-        });
-        connection.receive("getData", (data, responder) => {
-            console.log("incoming data", data);
+          }, 50);
+      }
+    });
+    connection.receive("getData", (data, responder) => {
+      console.log("incoming data", data);
 
-            responder.success({
-                ack: true,
-            });
-        });
-    },
-    onDisconnect() {
-        _connection = undefined;
-        MessageQueue.spy(false);
-        console.log("disconnected");
-    },
-    runInBackground() {
-        return false;
-    },
+      responder.success({
+        ack: true,
+      });
+    });
+  },
+  onDisconnect() {
+    _connection = undefined;
+    MessageQueue.spy(false);
+    console.log("disconnected");
+  },
+  runInBackground() {
+    return false;
+  },
 });
